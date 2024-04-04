67b2b0e07db16b1db29cc3f6a6eff0c4496ef8cd
Stephen Rayner <stephen.r.rayner@gmail.com>
2024-04-04 11:51:21 +0100

    fix: test with webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,36 @@
 /** @type {import('next').NextConfig} */
 let nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  },
+  distDir: process.env.NODE_ENV === "development" ? ".next/dev" : ".next/build",
+
   experimental: {
     serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+    externalDir: true
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.js\.map$/,
+      use: 'null-loader',
+    });
+
+
+
+    if (process.env.ENABLE_SOURCE_MAPS === "true") {
+      config.devtool = process.env.NODE_ENV === "development" ? "eval-source-map" : "source-map";
+    }
+
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false
+    };
+
+    return config;
   }
 };
 
